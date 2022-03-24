d4ab044d002efc8c056551ca0b15f4466df02596
semkeamsan <keamsan.sem@gmail.com>
2022-03-24 14:21:33 +0700

    Update

--- modules/Layout/user.blade.php
+++ modules/Layout/user.blade.php
@@ -97,6 +97,10 @@
         .bravo_user_profile > .container-fluid > .row-eq-height > .col-md-3 {
             min-height: 100vh !important;
         }
+        @php $main_color = setting_item('style_main_color','#103e79'); @endphp
+        .bravo_wrap .bravo_user_profile .sidebar-menu ul li.active.has-children>i:after, .bravo_wrap .bravo_user_profile .sidebar-menu ul li.active>a, .bravo_wrap .bravo_user_profile .sidebar-menu ul li.active>a i, .bravo_wrap .bravo_user_profile .sidebar-menu ul li:hover.has-children>i:after, .bravo_wrap .bravo_user_profile .sidebar-menu ul li:hover>a, .bravo_wrap .bravo_user_profile .sidebar-menu ul li:hover>a i {
+                color: {{ main_color }};
+        }
     </style>
     {{--Custom Style--}}
     <link href="{{ route('core.style.customCss') }}" rel="stylesheet">
